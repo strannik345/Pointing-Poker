6513907de8fe17eac9658ad58bb9e3b5f4ecf601
strannik345 <volodkovasya@gmail.com>
2021-09-06 11:39:42 +0300

    feat: add header and footer

--- pointing-poker/src/App.tsx
+++ pointing-poker/src/App.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import './app.css';
+import { StartPage } from './pages/start page/StartPage';
+
 function App() {
   return (
-    <div>
-      <header >
-        Pointing poker
+    <>
+      <header>
+        <div className="blue-line"></div>
+        <div className="light-blue-line"></div>
+        <div className="logo"></div>
       </header>
-    </div>
+      <main>
+        <StartPage/>
+      </main>      
+      <footer>
+        <ul className='footer-info'>
+          <li>2021</li>
+          <li>
+            <ul className='git-links'>
+              <li><div className="git-logo"></div><a href="https://github.com/IanaLit" target='_blank' rel="noreferrer">Iana Litvinova</a></li>
+              <li><div className="git-logo"></div><a href="https://github.com/strannik345" target='_blank' rel="noreferrer">Vasiliy Volodko</a></li>              
+            </ul>
+          </li>
+          <li className='rs-logo'><a href="https://rs.school/react/">rs-school-link</a></li>
+        </ul>
+      </footer>
+    </>
   );
 }
 
---
+++ pointing-poker/src/pages/start page/StartPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './startPage.css';
+
+export const StartPage: React.FC = () => {
+  return (
+    <>
+      <div className='main'>Hello</div>
+    </>
+  )
+}
