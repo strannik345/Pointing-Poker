37ed19ac4d05a9255f426e602d5483a443cde8a8
iana <litvinova.iana1212@gmail.com>
2021-09-09 10:14:06 +0700

    feat: add modals for kick player and create issue

--- pointing-poker/src/App.tsx
+++ pointing-poker/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { CreateIssueModal } from './components/createIssueModal/createIssueModal';
+import { KickModal } from './components/kickModal/kickModal';
 import { ScrumMaster } from './pages/lobby/scrumMaster/scrumMaster';
 import { TeamMember } from './pages/lobby/teamMember/teamMember';
 function App() {
   return (
     <div>
-      <ScrumMaster/>
+      <CreateIssueModal/>
     </div>
   );
 }
