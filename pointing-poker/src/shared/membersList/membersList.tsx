268cb532f95205657a773230668cf607da7948a4
strannik345 <volodkovasya@gmail.com>
2021-09-13 22:01:12 +0300

    feat: add isObserver to interface, addLink to loby

--- pointing-poker/src/shared/membersList/membersList.tsx
+++ pointing-poker/src/shared/membersList/membersList.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './../../pages/lobby/lobby.scss';
 import '@fontsource/ruda';
 import { Typography, Container } from '@material-ui/core';
 import { MemberCard } from '../memberCard/memberCard';
+import { Player } from '../../interfaces/player';
 export const MembersList: React.FC =()=> {
+    const [membersList, setMemberList] = useState<Player[]>([])
     return(<>
     <Container style={{width: '1000px', paddingTop: "100px"}}>
         <Typography className = "lobby--title lobby--title__primary">Members:</Typography>
