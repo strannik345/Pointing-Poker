3a2e026f68bc2bbb42d1c19053e9c700a52f440e
strannik345 <volodkovasya@gmail.com>
2021-09-13 22:50:07 +0300

    feat: add fetch for membersLobby

--- pointing-poker/src/shared/membersList/membersList.tsx
+++ pointing-poker/src/shared/membersList/membersList.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './../../pages/lobby/lobby.scss';
 import '@fontsource/ruda';
 import { Typography, Container } from '@material-ui/core';
@@ -6,6 +6,25 @@ import { MemberCard } from '../memberCard/memberCard';
 import { Player } from '../../interfaces/player';
 export const MembersList: React.FC =()=> {
     const [membersList, setMemberList] = useState<Player[]>([])
+    console.log(process.env.REACT_APP_HELLO)
+    useEffect(()=> {
+        subscribeMembers();
+    }, []);
+
+    const subscribeMembers = async () => {
+        try {
+            const response = await fetch('https://shielded-plains-14826.herokuapp.com/api/get-user');
+            const members: Player[] = await response.json();
+            setMemberList(prev => members );
+            console.log(membersList);
+            await subscribeMembers()
+        } catch(e) {
+            setTimeout(() => {
+                subscribeMembers();
+            }, 500);
+        }
+    }
+
     return(<>
     <Container style={{width: '1000px', paddingTop: "100px"}}>
         <Typography className = "lobby--title lobby--title__primary">Members:</Typography>
