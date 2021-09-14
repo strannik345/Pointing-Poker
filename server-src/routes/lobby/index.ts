e52a7058e27e9a9723ea8b3b93c04dabe7445386
strannik345 <volodkovasya@gmail.com>
2021-09-14 17:25:15 +0300

    feat: add strat game stop game api

--- server-src/routes/lobby/index.ts
+++ server-src/routes/lobby/index.ts
@@ -2,13 +2,34 @@ import express, { Router } from "express";
 import events from 'events';
 import { IUser } from "../../interfaces";
 
+interface StopGame {
+  gameNumber: number;
+}
+
 const emitter = new events.EventEmitter();
 
 const userList: IUser[] = [];
 
+const games: any[] = [];
+
 export const lobbyRouter = Router();
 
-lobbyRouter.get('/get-user', (req,res) => {
+lobbyRouter.get('/start-new-game', (req, res) => {
+  games.push({
+    gameURL: `/localhost:`,
+    gameNumber: games.length + 1,
+  });
+  res.json(games[games.length-1]);
+});
+
+lobbyRouter.post('/stop-game', (req, res) => {
+  const gameNumber: StopGame = req.body;  
+  games.filter(game => game.gameNumber!== gameNumber.gameNumber);
+  console.log(games, gameNumber);
+  res.status(200).end();  
+})
+
+lobbyRouter.get('/get-users', (req,res) => {
   emitter.once('newUser', () => {
     res.json(userList);
   })
