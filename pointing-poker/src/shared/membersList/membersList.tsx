735125b1ddba34db9c2d87a58c6eb06a9116c62b
strannik345 <volodkovasya@gmail.com>
2021-09-16 14:45:25 +0300

    feat: update user list done

--- pointing-poker/src/shared/membersList/membersList.tsx
+++ pointing-poker/src/shared/membersList/membersList.tsx
@@ -4,9 +4,24 @@ import '@fontsource/ruda';
 import { Typography, Container } from '@material-ui/core';
 import { MemberCard } from '../memberCard/memberCard';
 import { Player } from '../../interfaces/player';
+import { LobbyMemberCard } from '../memberCard/LobbyMemberCard';
+
+interface IGame {
+  gameID: number;
+  users: IUser[];
+}
+
+export interface IUser{
+  id: number;
+  avatar: string;
+  name: string;
+  position: string;
+  isBlocked: boolean;
+  isObserver: boolean;
+}
+
 export const MembersList: React.FC =()=> {
-    const [membersList, setMemberList] = useState<Player[]>([])
-    console.log(process.env.REACT_APP_SERVER)
+    const [memberList, setMemberList] = useState<IUser[]>([])
     useEffect(()=> {
         getCurrentUsers();
         subscribeMembers();
@@ -14,38 +29,37 @@ export const MembersList: React.FC =()=> {
 
     const getCurrentUsers = async () => {
         console.log('1');
-        const response = await fetch(`${process.env.REACT_APP_SERVER}/api/get-all-users?gameId=5`); //gameId from redux change then
-        const members: Player[] = await response.json();
-        setMemberList(members);
+        const response = await fetch(`${process.env.REACT_APP_SERVER}/api/get-all-users?gameID=3`); //gameId from redux change then
+        const members: IGame[] = await response.json();     
+        console.log(members[0].users);
+        setMemberList(prev => members[0].users);
+        console.log(memberList);
     }
 
     const subscribeMembers = async () => {
         console.log('2');
         try {             
-            const response = await fetch(`${process.env.REACT_APP_SERVER}/api/get-users?gameId=5`);
-            const members: Player[] = await response.json();
-            setMemberList(prev => members );
-            console.log(membersList);
+            const response = await fetch(`${process.env.REACT_APP_SERVER}/api/get-users?gameID=3`);
+            const members = await response.json();
+            setMemberList(members[0].users);
             await subscribeMembers()
         } catch(e) {
             setTimeout(() => {
                 subscribeMembers();
             }, 500);
         }
-    }
+    } 
 
-    return(<>
-    <Container style={{width: '1000px', paddingTop: "100px"}}>
-        <Typography className = "lobby--title lobby--title__primary">Members:</Typography>
-    </Container>
-    <Container className = "team-members">
-        <MemberCard isSmall={false}/>
-        <MemberCard isSmall={false}/>
-        <MemberCard isSmall={false}/>
-        <MemberCard isSmall={false}/>
-        <MemberCard isSmall={false}/>
-        <MemberCard isSmall={false}/>
-        <MemberCard isSmall={false}/>
-    </Container>
-    </>);
+    return(
+    <>
+      <Container style={{width: '1000px', paddingTop: "100px"}}>
+          <Typography className = "lobby--title lobby--title__primary">Members:</Typography>
+      </Container>
+      <Container className = "team-members">
+        {
+          memberList.map(member=> <LobbyMemberCard size={{isSmall:false}} userInfo={member}/>)
+        }        
+      </Container>
+    </>
+    );
 }
