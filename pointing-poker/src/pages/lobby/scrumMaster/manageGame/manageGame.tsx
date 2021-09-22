934e45ce22b58a047f98a7fbbd7715380e4f7602
iana <litvinova.iana1212@gmail.com>
2021-09-22 21:27:02 +0700

    refactor: moved the uploadImage function to a separate file

--- pointing-poker/src/pages/lobby/scrumMaster/manageGame/manageGame.tsx
+++ pointing-poker/src/pages/lobby/scrumMaster/manageGame/manageGame.tsx
@@ -21,7 +21,7 @@ export const ManageGame: React.FC =()=> {
         <Container className="manage-game">
             <Typography >Link to lobby:</Typography>
             <Box className="link-to-lobby" >
-                <Typography className = "link-to-lobby--text" style={{width: "-webkit-fill-available"}}>{gameID}</Typography>
+                <Typography className = "link-to-lobby--text" style={{height: "1.5rem", width: "-webkit-fill-available"}}>{gameID}</Typography>
                 <Button className = "button" variant="contained" color="primary">Copy</Button>
             </Box> 
             <Box className="manage-game-buttons">
